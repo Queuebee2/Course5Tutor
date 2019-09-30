1f3d6cf59f530b16fad91d3a4c788e1d44977af6
Milain Lambers <32010930+Queuebee2@users.noreply.github.com>
2019-09-30 17:19:53 +0200

    functions and os sep

--- scripts/fasta_scanner.py
+++ scripts/fasta_scanner.py
@@ -1,15 +1,21 @@
 
 from matplotlib import pyplot as plotter
+import os
+path_delim = os.sep
+
+IN_FILE = "in_data"+path_delim+"uniprot_thioredoxin_reviewed.fasta"
 
 original_names = open("original_names.txt", 'a')
 
 """ helper functions """
 
 def printDict(d):
+    # print keys and values from a dict next to eachother 
     for k, v in d.items():
         print(k,":",v)
         
 def scanFileFor(filepath, term):
+    # count occurences of a term (keyword) in a textfile
     count = 0
     with open(filepath, 'r'):
         for line in file:
@@ -22,50 +28,16 @@ def add():
     pass
 
 
-
-""" Main """
-
-
 interestingTerms = {"PE=3":0, "PE=1":0,
                     "PE=2":0, "PE=4":0, "PE=5":0}
 
 OSes = dict()
 
 
-# counter to use to interrupt the loop if you just want to browse through
-# uncomment the commented section in the file loop.
-dontDoTooMuch = 0
-
-with open("uniprot_thioredoxin_reviewed.fasta", 'r') as f:
-    PEs = 0 
-    for l in f:
-        line = l.strip()
-        
-                
-        if line.startswith(">"):
-            #print(line)
-            dontDoTooMuch+= 1
-
-            for part in line.split(" "):
-                if part.startswith("OS"):
-                    thing = part.split("OS=")[1]
-                    if thing in OSes:
-                        OSes[thing] +=1
-                    else:
-                        OSes[thing] = 1
-            
-            for term in interestingTerms:
-                if term in line:
-                    interestingTerms[term]+=1
-
-            #if dontDoTooMuch % 25 == 0:
-            #    b = input("press enter do 25 more")
-            #    if b in 'nostopb' and b != "":
-            #        break
-
-            
-
-def graphDict(d):
+def pieGraphDict(d):
+    # graphs the values in a dictionary with their key as label
+    # doesn't work with nested dicts.
+    
     values = []
     labels = []
     for k, v in d.items():
@@ -75,23 +47,53 @@ def graphDict(d):
     figureObject, axesObject = plotter.subplots()
 
     # Draw the pie chart
-
     axesObject.pie(values,
             labels=labels,
             autopct='%1.2f',
             startangle=90)
-  
-    # Aspect ratio - equal means pie is a circle
 
     axesObject.axis('equal')
-    
-
-    
     plotter.show()
 
+def main():
+    # counter to use to interrupt the loop if you just want to browse through
+    # uncomment the commented section in the file loop.
+    dontDoTooMuch = 0
+
+    with open(IN_FILE, 'r') as f:
+        PEs = 0 
+        for l in f:
+            line = l.strip()
+            
+                    
+            if line.startswith(">"):
+                #print(line)
+                dontDoTooMuch+= 1
+
+                for part in line.split(" "):
+                    if part.startswith("OS"):
+                        thing = part.split("OS=")[1]
+                        if thing in OSes:
+                            OSes[thing] +=1
+                        else:
+                            OSes[thing] = 1
+                
+                for term in interestingTerms:
+                    if term in line:
+                        interestingTerms[term]+=1
+
+                #if dontDoTooMuch % 25 == 0:
+                #    b = input("press enter do 25 more")
+                #    if b in 'nostopb' and b != "":
+                #        break
+
+                
+        
 
 printDict(interestingTerms)
-graphDict(interestingTerms)
+pieGraphDict(interestingTerms)
 printDict(OSes)
-graphDict(OSes)
+pieGraphDict(OSes)
+
+main()
 
