86ac76814b9ef67207a9c9430f19d638b6a424e1
Milain <32010930+Queuebee2@users.noreply.github.com>
2019-10-20 20:38:09 +0200

    elaborate on a TODO

--- scripts/main_script.py
+++ scripts/main_script.py
@@ -120,9 +120,21 @@ def main():
                 if line.startswith(">"):
                     if header != '':
                         
-                        pass
-# TODO              uniprot_ID = line.split(delim)[index]
-# TODO              check whether its actually a uniprot id, otherwise skip?
+                        
+# TODO                  detect which database it comes from?        https://en.wikipedia.org/wiki/FASTA_format#NCBI_identifiers    
+                        # SWISS-PROT 	    sp|accession|name       ''
+                        # PDB 	            pdb|entry|chain         ''
+                        # Refseq protein    >NP_id                  https://www.ncbi.nlm.nih.gov/books/NBK50679/
+                        
+
+                        # check if it already exists in our database (?? will this be costly?)
+
+                        # Do we care about redundant sequences?
+                        # Proposal: remove junction table and add 3 db-id columns to
+                        # primary database table 
+                        # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5225397/
+                        # http://weizhongli-lab.org/cd-hit/
+
 
                     # prep loop for next sequence
                     header = line
