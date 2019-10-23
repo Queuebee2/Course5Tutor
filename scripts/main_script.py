eab290f3c9e431ac1f0edeaca8cfca56ccd2a9fa
Milain <32010930+Queuebee2@users.noreply.github.com>
2019-10-24 00:49:53 +0200

    implement seq_from_hmm in main
    no imports, because I don't control those either

--- scripts/main_script.py
+++ scripts/main_script.py
@@ -11,6 +11,7 @@ from DbConnector import DbConnector
 # regex object used in find_target_pattern(string):
 TARGET_PATTERN = re.compile('[C]..[C]')
 
+# filenames
 FASTA_FILENAME = 'fasta.fa'
 MSA_FILENAME = 'msa.msa'
 HMM_FILENAME = 'hmm.hmm'
@@ -116,6 +117,11 @@ def search_uniprot(uniprot_handle, uniprot_entry_id, columns, verbose=False):
     if verbose:
         print("searched", uniprot_entry_id)
 
+#TODO parse search_result here (not really needed probably)
+# could just do an insert with columns=columns, values=search_result.split
+
+#TODO SQL insert here
+
 
 def fetch_fasta_from_uniprot(uniprot_handle, acession, verbose=False):
     """should return the header+sequence of a swissprot entry in
@@ -161,13 +167,8 @@ def fetch_fasta_from_uniprot(uniprot_handle, acession, verbose=False):
         # gosh darnit
         raise UrgentUnknownError('type fasta_str:' + str(type(fasta_str)) +
                                  ' acession id:' + str(acession))
-        
 
-    
-#TODO parse search_result here (not really needed probably)
-# could just do an insert with columns=columns, values=search_result.split
 
-#TODO SQL insert here
 def doBlast(filename=BLAST_OUTPUT , **kwargs):
     # take a sequence to exec5ute a blastn against the nt database
     # to do : add blasttype, db and matrix parameters
@@ -181,6 +182,41 @@ def doBlast(filename=BLAST_OUTPUT , **kwargs):
     # write results ( xml-string )
     blast_file.write(result_handle.read())
     
+def test_headers(l):
+    """helper function for seq_from_hmm"""
+    # when we find all these chars in a line, the matrix starts
+    for char in "ACDEFGHIKLMNPQRSTVWY":
+        if char not in l:
+            return False
+    return True
+
+def seq_from_hmm(hmm_file=HMM_FILE):
+    
+    with open(hmm_file, 'r') as f:
+        
+        did_headers = False
+        markov_word = ''
+        
+        for i, line in enumerate(f):
+            if not did_headers:
+                didHeaders = test_headers(line)
+            else:
+                try:
+                    # the end of some lines look like      nnn x - - -
+                    # with nn being numbers and x being a letter for a protein
+                    # - being literally dashes, not sure what other possibilities
+                    # are, yet.
+                    # by splitting the whole line and taking the [-4]th element
+                    # the character we obtain seems to be part of the consensus
+                    # pattern
+                    w = line.split()[-4]
+                    if len(w) == 1 and w in PROTEIN_SYMBOLS:
+                        markov_word += w
+                except Exception as e:
+                    print(e , "on row, ",i)
+                
+    return markov_word
+
 
 
 def main():
