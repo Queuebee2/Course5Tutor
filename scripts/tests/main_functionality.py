5a05950a4858a75e3be234011c607222f6ed127d
Milain Lambers <32010930+Queuebee2@users.noreply.github.com>
2019-10-24 13:48:23 +0200

    move functions from main_script to main_test
    due to a weird error. Trying this to get it to work.

--- scripts/tests/main_functionality.py
+++ scripts/tests/main_functionality.py
@@ -1,8 +1,56 @@
-import sys
+import subprocess
+from sys import argv
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from main_script import create_msa_mafft, create_hmm
+def create_msa_mafft(fasta_filename, msa_destination_filename, verbose=False):
+    """calls 'mafft' through the shell to create a
+       multiple sequence alignment (msa_filename)
+
+    ARGS:
+        fasta_filename: string
+            relative path (just a filename) of a file containing fasta
+            sequences to be aligned
+
+        msa_destination_filename: string
+            relative path in which to save the multiple sequence alignment
+
+    """
+
+    # Check whether msa has already been made
+    if os.path.isfile(msa_destination_filename):
+        pass
+    # Execute command if msa hasn't been made before
+    else:
+        cmd = "mafft " + fasta_filename + " > " + msa_destination_filename
+        e = subprocess.check_call(cmd, shell=True)
+        if verbose:  print(e)
+    return
+
+def create_hmm(msa_filename, hmm_destination_filename, verbose=False):
+    """calls 'hmmbuild' through the shell to create a hmm profile
+       from a multiple sequence alignment (msa_filename)
+
+    ARGS:
+        msa_filename: string
+            relative path (just a filename) of
+            a file containing a valid MSA
+
+        hmm_destination_filename: string
+            relative path in which to create
+            a hmm profile using hmmer
+
+    """
+
+    # Check whether hmm has already been made
+    if os.path.isfile(hmm_destination_filename):
+        pass
+    # Execute command if hmm hasn't been made before
+    else:
+        cmd = "hmmbuild " + hmm_destination_filename + " " + msa_filename
+        e = subprocess.check_call(cmd, shell=True)
+        if verbose:  print(e)
+    return
+
 
 if __name__ == '__main__':
     test_fasta_filename = "multiple_fasta_sequences.fasta"
