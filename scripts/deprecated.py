ac957c59b45c3599ed1bb62cf4f29f3c9ecbc5ee
Milain <32010930+Queuebee2@users.noreply.github.com>
2019-10-24 23:29:37 +0200

    selfdeprecation I
    removing comepletely useless methods from the main script

---
+++ scripts/deprecated.py
@@ -0,0 +1,41 @@
+
+
+
+# deprecated because we shouldnt extract consensus patterns from hmm files
+PROTEIN_SYMBOLS = 'ACDEFGHIKLMNPQRSTVWYadefghiklmnpqrstvwy'
+
+
+def test_headers(l):
+    """helper function for seq_from_hmm"""
+    # when we find all these chars in a line, the matrix starts
+    for char in "ACDEFGHIKLMNPQRSTVWY":
+        if char not in l:
+            return False
+    return True
+
+def seq_from_hmm(hmm_file=HMM_FILE):
+    
+    with open(hmm_file, 'r') as f:
+        
+        did_headers = False
+        markov_word = ''
+        
+        for i, line in enumerate(f):
+            if not did_headers:
+                didHeaders = test_headers(line)
+            else:
+                try:
+                    # the end of some lines look like      nnn x - - -
+                    # with nn being numbers and x being a letter for a protein
+                    # - being literally dashes, not sure what other possibilities
+                    # are, yet.
+                    # by splitting the whole line and taking the [-4]th element
+                    # the character we obtain seems to be part of the consensus
+                    # pattern
+                    w = line.split()[-4]
+                    if len(w) == 1 and w in PROTEIN_SYMBOLS:
+                        markov_word += w
+                except Exception as e:
+                    print(e , "on row, ",i)
+                
+    return markov_word
