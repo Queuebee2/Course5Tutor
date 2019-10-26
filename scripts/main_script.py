99d9c482bce088ac9e2c582e808052a56cdc76f0
Milain <32010930+Queuebee2@users.noreply.github.com>
2019-10-26 09:21:10 +0200

    might almost be shipping time

--- scripts/main_script.py
+++ scripts/main_script.py
@@ -15,7 +15,7 @@ from DbConnector import DbConnector
 TARGET_PATTERN = re.compile('[C]..[C]')
 
 # entry-point filenames
-FASTA_DATABASE = '/home/queue/SwissProt/uniprot_sprot.fasta.gz'
+FASTA_DATABASE = '/home/queue/SwissProt/uniprot_sprot.fasta.gz'#must be zip.gz!
 
 # temp filenames
 FASTA_FILENAME = 'fasta.fa'
@@ -157,6 +157,35 @@ def do_hmm_search(hmm_filename=HMM_FILENAME,
     return
 
 
+def fetch_fasta_from_local_zip_db(accession, local_zip_db_name=FASTA_DATABASE):
+    """ hacky function to gather header + sequence by acession in a
+        downloaded database file (zipped fasta)
+        """
+    with gzip.open(gzip_db_location, 'rt') as db_as_zipfile:
+        header = ''
+        seq = ''
+        for line in db_as_zipfile:
+            if line.startswith(">") and header != '':
+                print(header, "\n",seq)
+                if seq != '':
+                    return header, seq
+                else:
+                    # TODO MAKE CUSTOM EXCEPTION
+                    raise Exception("seq not filled in"+\
+                        "fetch_fasta_from_local_zip_db"!)
+            
+            if accession in line:
+                header = line.strip("\n")
+
+            if header != '' and not line.startswith(">"):
+                seq += line.strip("\n")
+
+        # TODO MAKE CUSTOM EXCEPTION
+        raise Exception("something weird happened in"+\
+                        "fetch_fasta_from_local_zip_db")
+                
+
+# prolly not gonna use lol
 def fetch_fasta_from_uniprot(uniprot_handle, acession, verbose=False):
     """should return the header+sequence of a swissprot entry in
        fasta format, but otherwise lets you know it didn't.
@@ -232,11 +261,17 @@ def get_uniprot_stuff(uniprot_handle, acession, columns_list=DEFAULT_SELECTION,
 
 def iterate_hmm_search_tab_results(filename=HMM_SEARCH_TAB_OUTPUT_FILENAME
                                    , verbose=False):
-    """ iterate over hmmsearch3 tab output using SearchIO.parse
+    """ GENERATOR!!!
+        iterate over hmmsearch3 tab output using SearchIO.parse
+
+    yields:
+        Bio.< ? ? >.Hit.id
+        Bio.< ? ? >.Hit.evalue
 
     """
-
-    results
+    result = next(SearchIO.parse(filename, 'hmmer3-tab'))
+    for hit in result.hits:
+        yield hit.id, hit.evalue
     
     
 def make_obscure_SQL_part(d):
@@ -247,9 +282,12 @@ def make_obscure_SQL_part(d):
     return "'" + "','".join([d[k] for k in keys_in_order]) + "'"
         
 
-def important_mainloop():
+def important_mainloop(verbose=True):
+    # todo set verbose default to False again one day or another im gonna find ya im gonna getcha getcha getcha getcha
+
     # create an object to handle communications with mySQL database
-    our_database_handle = DbConnector()
+    db = DbConnector() 
+    
 
     # create uniprot handle object
     # todo: find out if 'handle' is the right terminology here
@@ -267,42 +305,59 @@ def important_mainloop():
     # found
     running = True
     iteration = 0 # TODO function to get iteration from db
-        
+    
     # 'running' loop
     while running:
         
         # msa
-        create_msa_mafft()
+        
+        create_msa_mafft() ;if verbose: print("main: trying to create msa...")
 
         # hmm
-        create_hmm()
+        create_hmm() ;if verbose: print("main: trying to create phmm...")
 
         # hmm search
-        do_hmm_search()
+        do_hmm_search() ;if verbose: print("main: trying to do hmmsearch...")
 #TODO
 
         # iterate hmm search results
-            # find out what the acession id is
-            # "SELECT * FROM PROTEIN WHERE ID = '{}'".format(id)
-            # if select yields results: skip
-            # else:
-                # (not sure this is necessary ) - get whole fasta sequence
-                # guess it is, since we need to add to main fasta
-                # fetch_fasta_from_uniprot(uniprot_handle, acession)
-                
-                # pos_2c = find_target_pattern(sequence:str)
-                # GO_STUFF_D = get_uniprot_stuff(uniprot_handle, acession)
+        innerLoop = True
 
-                # obscure_GO_stuff = make_obscure_SQL_part(GO_STUFF_D)
-                # INSERT INTO PROTEIN VALUES (VALUES)
-    """INSERT INTO protein VALUES (NULL, # null id autoincrement
-                            'testheader',  # text
-                            'tesequence',  # text
-                            0,  # int iteration
-                            'testgobioprocesstext', 
-                            'testgocellcomptext',
-                            'tesgomolfunctext',
-                            1337); # int pos2_c """ 
+        while innerLoop:
+            identifier, evalue = iterate_hmm_search_tab_results()
+
+            actual_id = identifier.split("|")[1]
+
+            # sql select to check if it exists in our db
+            if db.exists_protein(actual_id):
+                continue  # skip (doesnt check if other values filled though)
+            else:
+    
+                # deprecated as we have a local database now
+                # fetch_fasta_from_uniprot(uniprot_handle, acession)
+
+
+                # fetch header, fasta from local database
+                header, seq = fetch_fasta_from_local_zip_db(actual_id)
+
+                pos_2c = find_target_pattern(seq)
+                GO_STUFF_D = get_uniprot_stuff(uniprot_handle, acession)
+                obscure_GO_stuff = make_obscure_SQL_part(GO_STUFF_D)
+                query_dict = {"id":
+                query = f"""INSERT INTO PROTEIN VALUES(
+                        NULL,
+                        '{actual_id}',
+                        '{header}',
+                        '{seq}',
+                        {iteration},
+                        {GO_STUF_D['go(biological process)'},
+                        {GO_STUF_D['go(cellular component)'},
+                        {GO_STUF_D['go(molecular function)'},
+                        {pos_2c});
+                        """
+
+                db.commit_query(query)
+
         
         # repeat?
     
