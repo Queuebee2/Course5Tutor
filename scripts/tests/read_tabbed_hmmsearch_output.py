fe68861e42a99ef594ffe6b57c5b08f8c39e531f
Milain <32010930+Queuebee2@users.noreply.github.com>
2019-10-26 07:55:23 +0200

    playing with HmmerIO

--- scripts/tests/read_tabbed_hmmsearch_output.py
+++ scripts/tests/read_tabbed_hmmsearch_output.py
@@ -4,19 +4,22 @@ from Bio import SearchIO
 usedcmd ="""hmmsearch --tblout myhits.tbl --acc --noali hmm_file.hmm /home/queue/SwissProt/uniprot_sprot.fasta.gz                                                                                           
 
 """
-tabs = SearchIO.parse('myhits.tbl', 'hmmer3-tab')
+result = next(SearchIO.parse('myhits.tbl', 'hmmer3-tab'))
 
 
 c = 0
-for result in tabs:
 
-    if c < 2:
+for hit in result.hits:
 
-        print(result)
-        print(dir(result))
-        
+
+    if c < 10:
+        print("ID:",hit.id)
+        print("ACESS:",hit.acession)
+        print("EVAL:",hit.evalue) 
 
     else:
+        print(hit.id)
+        print(dir(hit))
         break
-
+        
     c+=1
